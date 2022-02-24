eedd59f07813fe132aeba007abe0729c472b75d7
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-02-24 10:01:23 +0600

    font added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import Home from "./Pages/Home/Home";
 function App() {
   return (
     <div className="App">
-      <h2 className="text-center">Lorem ipsum dolor sit amet.</h2>
+      <Home></Home>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div id='Home'>
+            I am home
+        </div>
+    );
+}
+
+export default Home;
